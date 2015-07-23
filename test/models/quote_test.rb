fbeca581b0e7e4ab821229abe452818c0eef5aa4
benzegreatest <benchinoy@gmail.com>
2015-07-23 00:16:53 +0000

    Adding testing dev

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+  
+  test "unique_tag" do
+  	quote = Quote.create(:author => 'Ben Chinoy', :saying => 'You only live once')
+  	expected = 'BC#' + quote.id.to_s
+  	actual = quote.unique_tag
+  	assert_equal expected, actual
+  end
+
 end
